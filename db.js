45f6801c68db8894b34ba7a8f15da1b2c57da970
Pradeep2027 <shabadpradeep1@gmail.com>
2026-04-15 17:30:35 +0530

    Initial Commit

---
+++ db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const mongoURL = "mongodb://localhost:27017"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURL).then(() => console.log("Connected to Mongo successfully")).catch((e) => console.log(e.messsage));
+}
+
+module.exports = connectToMongo;
