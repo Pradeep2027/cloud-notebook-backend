ac10df1dd9489039f3295d357e228b63bd279c24
Pradeep2027 <shabadpradeep1@gmail.com>
2026-04-22 18:15:07 +0530

    Built Add a note and fetch notes feature

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth.js'))
-app.use('/api/notes', require('./routes/notes.js'))
+app.use('/api/notes', require('./routes/note.js'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
