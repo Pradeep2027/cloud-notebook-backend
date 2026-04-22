b4e569547886d733030fa4f73924ccf6a503ce56
Pradeep2027 <shabadpradeep1@gmail.com>
2026-04-22 12:40:57 +0530

    Added Login route

--- routes/auth.js
+++ routes/auth.js
@@ -37,8 +37,40 @@ router.post("/createuser", [
       res.json({authToken});
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal server error");
     }
-  },
+  });
+
+// Authenticate a User using : POST "/api/auth/login". No login is required
+router.post("/login", [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({ min: 5 })
+  ], async (req, res) => {
+    // if there are any errors return bad requests and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password} = req.body;
+    try {
+      let user = await User.findOne({email});
+      if (!user)    {
+        return res.status(400).json({ error: "Invalid username or password" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Invalid username or password" });
+      }
+      const data = {
+        user: {id: user.id}
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({authToken});
+    } catch(error)  {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
 );
 module.exports = router;
